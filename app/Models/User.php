507de1a2ccf947f5c3b813dedc1e3e2b6f74b08d
Revprm <revy.pramana@gmail.com>
2025-01-02 06:40:10 +0700

    feat: add Report model and establish user association

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Report;
 
 class User extends Authenticatable
 {
@@ -50,5 +51,10 @@ class User extends Authenticatable
     public function predictionHistories(): HasMany
     {
         return $this->hasMany(PredictionHistory::class);
+    }
+
+    public function reports(): HasMany
+    {
+        return $this->hasMany(Report::class);
     }
 }
